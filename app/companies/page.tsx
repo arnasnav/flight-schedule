924d8264f1d65ada933677265ce008d01625c3de
arnasnav <arnas.navaruckis@panko.lt>
2026-05-07 16:19:36 +0300

    Refs: #12

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -1,8 +1,8 @@
-import { Companies } from "@/components/companies/company-page"
+import { Companies } from "@/components/companies/company"
 import { getApi } from "@/utils/server-api"
 import { ICompany } from "@/models/company-model"
 
 export default async function Page() {
   const companies = await getApi<ICompany[]>("/api/companies")
-  return <Companies company={companies ?? []} />
+  return <Companies companies={companies ?? []} />
 }
