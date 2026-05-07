04b98b5d3c4bb3f63fd0d1dc7e51ce947c2eff71
arnasnav <arnas.navaruckis@panko.lt>
2026-05-07 16:21:35 +0300

    Refs: #12

--- app/airports/page.tsx
+++ app/airports/page.tsx
@@ -1,8 +1,8 @@
-import { Airports } from "@/components/flights/airport-page"
+import { Airports } from "@/components/flights/airport"
 import { getApi } from "@/utils/server-api"
 import { IAirport } from "@/models/airport-model"
 
 export default async function Page() {
   const airport = await getApi<IAirport[]>("/api/airports")
-  return <Airports airport={airport ?? []} />
+  return <Airports airports={airport ?? []} />
 }
