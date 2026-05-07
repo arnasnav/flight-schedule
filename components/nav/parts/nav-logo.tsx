7797111bbc78acd7c38cf9ae435f2ba0c2582ab5
arnasnav <arnas.navaruckis@panko.lt>
2026-05-07 16:40:09 +0300

    refactor: switch icons from heroicons to lucide-react

--- components/nav/parts/nav-logo.tsx
+++ components/nav/parts/nav-logo.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link"
-import { PaperAirplaneIcon } from "@heroicons/react/24/outline"
+import { Plane } from "lucide-react"
 
 export function NavLogo() {
   return (
     <Link href="/" className="flex items-center space-x-3">
       <div className="flex gap-x-2">
-        <PaperAirplaneIcon className="h-8 w-8 stroke-orange-700" />
+        <Plane className="h-8 w-8 stroke-orange-700" />
         <div className="text-2xl text-orange-700 font-bold">
           Lėktuvų tvarkaraštis
         </div>
